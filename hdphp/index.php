4f74185763386af4d997f00b65154c5f636d4d43
5856022860 <2286058560@qq.com>
2015-04-15 13:32:21 +0800

    创建app应用

--- hdphp/index.php
+++ hdphp/index.php
@@ -4,8 +4,8 @@ define('DEBUG',True);
 //显示DEBUG面板
 define('DEBUG_TOOL',true);
 //指量创建模块
-define('MODULE_LIST','Index,Admin');
+// define('MODULE_LIST','Index,Admin');
 //应用目录
-define('APP_PATH','Application/');
+define('APP_PATH','App/');
 //引入框架
 require 'hdphp/hdphp.php';
